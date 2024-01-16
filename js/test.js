7d1b81943d477316630e73f45682a9bdb73bafd3
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-16 23:28:42 +0900

    JSのexortの使い方について

---
+++ js/test.js
@@ -0,0 +1,5 @@
+// ファイル test.js
+const k = 12;
+export default k;
+
+export let j = 13;
---
+++ js/test2.js
@@ -0,0 +1,5 @@
+import m from "./test.js";
+import { j } from "./test.js";
+
+console.log(m);
+console.log(j);
