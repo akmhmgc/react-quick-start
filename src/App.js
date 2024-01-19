af686f82041dfd7a31721b806d4b3da0508bdb62
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-19 22:48:42 +0900

    Refactor Board component and add Game component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquare] = useState(Array(9).fill(null));
+
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) return;
 
@@ -19,8 +18,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquare(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares)
   }
   const winner = calculateWinner(squares);
   let status;
@@ -48,6 +46,29 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // スプレッド構文
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}  />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
