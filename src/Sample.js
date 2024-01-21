8e85ad4b046fd6ec523b9a9d39ea422e38d2852c
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-21 14:52:11 +0900

    Refactor Example component to use class-based syntax

--- src/Sample.js
+++ src/Sample.js
@@ -1,22 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-export default function Example() {
-  const [count, setCount] = useState(0);
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
 
-  // Similar to componentDidMount and componentDidUpdate:
-  // 副作用のある処理を実行
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.getElementsByClassName("title")[0].innerHTML = `You clicked ${count} times`;
-  });
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//     </div>
+//   );
+// }
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <p className='title'>title</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
+
+export default Example;
