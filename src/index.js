4b6ef86fccc043b4d7c456ff42f1ebc37a8c88ef
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-26 09:00:31 +0900

    Add Mock.js file and update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-// import App from "./App";
-import App from "./Sample";
+import App from "./Mock";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
