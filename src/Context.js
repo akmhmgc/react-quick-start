d2b000b76ccd242f4d72a22817b5a50d184d6ba6
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-27 14:00:27 +0900

    Refactor theme handling and add dark mode support

--- src/Context.js
+++ src/Context.js
@@ -1,17 +1,52 @@
-import { useContext, createContext } from "react";
+import { createContext, useContext, useState } from 'react';
 
-const Theme = createContext("light");
+const ThemeContext = createContext(null);
 
-function DisplayTheme() {
-  const theme = useContext(Theme);
-  return <p>Active theme: {theme}</p>;
+export default function MyApp() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Form />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light')
+          }}
+        />
+        Use dark mode
+      </label>
+    </ThemeContext.Provider>
+  )
 }
 
-// ...
-export default function App() {
+function Form({ children }) {
   return (
-    <Theme.Provider value="light">
-        <DisplayTheme />
-    </Theme.Provider>
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'button-' + theme;
+  return (
+    <button className={className}>
+      {children}
+    </button>
   );
 }
