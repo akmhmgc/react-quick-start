b68a9c2ccf68a89f2395883d7fbdfdbcbeb09084
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-27 13:22:24 +0900

    Add reducer for counter component

---
+++ src/Reducer.js
@@ -0,0 +1,26 @@
+import { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment': return state + 1;
+    case 'decrement': return state - 1;
+    case 'reset': return 0;
+    default: throw new Error('Unexpected action');
+  }
+};
+
+function Counter() {
+  // 現在のステートとアクションをトリガするdispatch関数を返します。
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      {count}
+      <button onClick={() => dispatch('increment')}>+1</button>
+      <button onClick={() => dispatch('decrement')}>-1</button>
+      <button onClick={() => dispatch('reset')}>reset</button>
+    </div>
+  );
+}
+
+export default Counter;
