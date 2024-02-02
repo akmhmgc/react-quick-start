7d2de7843b04566c479fea5a4d67e7bc4ca2046e
akmhmgc <akimuhamaguchi@icloud.com>
2024-02-02 20:52:56 +0900

    Add missing line break to console.log statement

--- js/prototype.js
+++ js/prototype.js
@@ -3,3 +3,4 @@ Array.prototype.toString = () => 999;
 const numbers = [1, 2, 3];
 // `Array.prototype.toString`が定義されているため、`Object.prototype.toString`とは異なる出力形式となる
 console.log(numbers.toString()); // => "1,2,3"
+
