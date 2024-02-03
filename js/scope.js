f49978b6a2cd388f4330376f658ecbfd0883eb47
akmhmgc <akimuhamaguchi@icloud.com>
2024-02-03 11:30:11 +0900

    Refactor scope.js to use a closure for creating a counter

--- js/scope.js
+++ js/scope.js
@@ -1,13 +1,18 @@
-// エラーになる
-// console.log(x);
-// {
-//   const x = "varのx";
-// }
-// console.log(x);
+const createCounter = () => {
+  let count = 0;
+  return function increment() {
+    // `increment`関数は`createCounter`関数のスコープに定義された`変数`count`を参照している
+    count = count + 1;
+    return count;
+  };
+};
+// createCounter()の実行結果は、内側で定義されていた`increment`関数
 
+// メモリ上から解放されるので値は保持されない
+console.log(createCounter()()); // => 1
+console.log(createCounter()()); // => 1
 
-console.log(y); //=> undefined
-{
-  var y = "varのy";
-}
-console.log(y)  //=> "varのy"
+// 参照され続けるので値が保持される
+const myCounter = createCounter();
+console.log(myCounter()); // => 1
+console.log(myCounter()); // => 2
