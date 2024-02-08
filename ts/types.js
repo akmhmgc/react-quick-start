69ab251782b0214d86395244080113a52a8ed951
akmhmgc <akimuhamaguchi@icloud.com>
2024-02-08 23:34:07 +0900

    Refactor types.js and types.ts files

--- ts/types.js
+++ ts/types.js
@@ -1,9 +1,22 @@
 "use strict";
 // tuple
 var point = [7, 5];
-var entries = Object.entries({ a: 1, b: 2 });
 var toStr = function (num) { return String(num); };
+// 関数を引数にした場合、関数の型も定義することができる
 var hoge = function (callback) {
     return callback(10);
 };
-console.log(hoge(toStr));
+// optimal parameter
+var hey = function (word) {
+    var wrd = word !== null && word !== void 0 ? word : "hoge";
+    return wrd;
+};
+// rest parameters
+function joinNumbers() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    return nums.join('-');
+}
+joinNumbers(1, 2, 3);
--- ts/types.ts
+++ ts/types.ts
@@ -1,8 +1,6 @@
 // tuple
 let point: [number, number] = [7, 5];
 
-const entries = Object.entries({ a: 1, b: 2 });
-
 const toStr = (num: number): string => String(num);
 
 // 関数を引数にした場合、関数の型も定義することができる
@@ -10,4 +8,15 @@ const hoge = (callback: (num: number) => string) => {
   return callback(10);
 };
 
-console.log(hoge(toStr));
+// optimal parameter
+const hey = (word?: string): string => {
+  const wrd = word ?? "hoge";
+  return wrd;
+};
+
+// rest parameters
+function joinNumbers(...nums: number[]): string {
+  return nums.join('-');
+}
+
+joinNumbers(1,2,3)
